d054c8e831a23cefa10c6dc0d32649d382b4aaf7
justmalysa <justynamalysa98@gmail.com>
2021-03-25 20:59:01 +0100

    Add test case for shared elevator
    Also renamed some test cases to better reflect
    their intention.

--- tests/test_cases.c
+++ tests/test_cases.c
@@ -57,7 +57,7 @@ void test_elevator_floor_reached_check(void)
     TEST_ASSERT(elevator_floor_reached_check(test_elev_index));
 }
 
-void test_elevator_single_call_and_steps(void)
+void test_one_elevator_call_without_ride(void)
 {
     int test_user_floor = 4;
     int test_direction = 1;
@@ -75,7 +75,7 @@ void test_elevator_single_call_and_steps(void)
     TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_floor));
 }
 
-void test_elevator_single_call_steps_and_ride(void)
+void test_one_elevator_with_default_values(void)
 {
     int test_user_floor = 4;
     int test_user_target_floor = 6;
@@ -99,7 +99,7 @@ void test_elevator_single_call_steps_and_ride(void)
     TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_target_floor));
 }
 
-void test_elevator_three_calls_steps_and_ride(void)
+void test_three_elevators_with_default_values(void)
 {
     int test_user_floor_1 = 0;
     int test_user_target_floor_1 = 4;
@@ -160,7 +160,7 @@ void test_elevator_three_calls_steps_and_ride(void)
     TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_target_floor_2));
 }
 
-void test_elevator_single_call_steps_and_ride_with_initial_values(void)
+void test_one_elevator_with_preset_values(void)
 {
     int test_user_floor = 5;
     int test_user_target_floor = 7;
@@ -184,6 +184,68 @@ void test_elevator_single_call_steps_and_ride_with_initial_values(void)
     TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_target_floor));
 }
 
+void test_three_elevators_with_preset_values(void)
+{
+    int test_user_floor_1 = 8;
+    int test_user_target_floor_1 = 6;
+    int test_direction_1 = 0;
+
+    int test_user_floor_2 = 2;
+    int test_user_target_floor_2 = 3;
+    int test_direction_2 = 1;
+
+    elevator_init();
+    elevator_arr[4].current_floor = 7;
+    elevator_arr[4].target_floor = 9;
+    elevator_arr[4].requested_floors[9] = true;
+
+    elevator_arr[0].current_floor = 0;
+    elevator_arr[0].target_floor = 4;
+    elevator_arr[0].requested_floors[4] = true;
+
+    elevator_arr[7].current_floor = 10;
+    elevator_arr[7].target_floor = 5;
+    elevator_arr[7].requested_floors[5] = true;
+
+    elevator_call(test_user_floor_1, test_direction_1);
+    elevator_call(test_user_floor_2, test_direction_2);
+
+    elevator_step();
+    TEST_ASSERT_EQUAL(elevator_arr[4].current_floor, 8);
+    TEST_ASSERT_EQUAL(elevator_arr[0].current_floor, 1);
+    TEST_ASSERT_EQUAL(elevator_arr[7].current_floor, 9);
+
+    elevator_step();
+    TEST_ASSERT_EQUAL(elevator_arr[4].current_floor, 9);
+    TEST_ASSERT_EQUAL(elevator_arr[0].current_floor, 2);
+    TEST_ASSERT_EQUAL(elevator_arr[7].current_floor, 8);
+
+    TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_floor_1));
+    TEST_ASSERT(elevator_ride(test_user_floor_1, test_user_target_floor_1));
+
+    TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_floor_2));
+
+    TEST_ASSERT(elevator_ride(test_user_floor_2, test_user_target_floor_2));
+
+    TEST_ASSERT_FALSE(elevator_on_floor_check(test_user_target_floor_2));
+
+    elevator_step();
+    TEST_ASSERT_EQUAL(elevator_arr[0].current_floor, 3);
+    TEST_ASSERT_EQUAL(elevator_arr[7].current_floor, 7);
+
+    TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_target_floor_2));
+    TEST_ASSERT_FALSE(elevator_on_floor_check(test_user_target_floor_1));
+
+    elevator_step();
+    TEST_ASSERT_EQUAL(elevator_arr[0].current_floor, 4);
+    TEST_ASSERT_EQUAL(elevator_arr[7].current_floor, 6);
+
+    TEST_ASSERT_TRUE(elevator_on_floor_check(test_user_target_floor_1));
+
+    elevator_step();
+    TEST_ASSERT_EQUAL(elevator_arr[7].current_floor, 5);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -193,10 +255,11 @@ int main(void)
     RUN_TEST(test_elevator_ride);
     RUN_TEST(test_elevator_step);
     RUN_TEST(test_elevator_floor_reached_check);
-    RUN_TEST(test_elevator_single_call_and_steps);
-    RUN_TEST(test_elevator_single_call_steps_and_ride);
-    RUN_TEST(test_elevator_three_calls_steps_and_ride);
-    RUN_TEST(test_elevator_single_call_steps_and_ride_with_initial_values);
+    RUN_TEST(test_one_elevator_call_without_ride);
+    RUN_TEST(test_one_elevator_with_default_values);
+    RUN_TEST(test_three_elevators_with_default_values);
+    RUN_TEST(test_one_elevator_with_preset_values);
+    RUN_TEST(test_three_elevators_with_preset_values);
 
     return UNITY_END();
 }
